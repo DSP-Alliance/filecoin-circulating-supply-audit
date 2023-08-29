a1473dca560b1c408fe727cfae22910eeb8bfccd
isp <53378653+isp@users.noreply.github.com>
2023-08-29 15:19:33 -0500

    Update supply.py
    Corrected calculations

--- supply.py
+++ supply.py
@@ -23,6 +23,19 @@ def load_film_prices_from_csv(csv_filename):
             fil_prices[original_date_object] = fil_price
     return fil_prices
 
+# This function will return the total vested amount for the foundation or labs until a given date
+def calculate_vested_amount(start_date, current_date, total_amount, vesting_years):
+    days_since_start = (current_date - start_date).days
+    total_days = vesting_years * 365
+    vested_amount = min(total_amount * days_since_start / total_days, total_amount)
+    return vested_amount
+
+def daily_vested_amount(start_date, current_date, total_amount, vesting_years):
+    days_since_start = (current_date - start_date).days
+    yesterday_vested = calculate_vested_amount(start_date, current_date - timedelta(days=1), total_amount, vesting_years)
+    today_vested = calculate_vested_amount(start_date, current_date, total_amount, vesting_years)
+    return today_vested - yesterday_vested
+
 # Function to process data for a specific date
 def process_data_for_date(date, prev_ff_pl_team_unlock):
     formatted_date = date.strftime('%Y-%m-%d')  # Convert date to ISO 8601 format
@@ -30,13 +43,11 @@ def process_data_for_date(date, prev_ff_pl_team_unlock):
         'start_date': formatted_date,
         'end_date': formatted_date
     }
-
     max_retries = 3  # setting a maximum retry limit
     retries = 0
 
     while retries < max_retries:
         print(f"Fetching data for date: {formatted_date}")
-
         try:
             response = requests.get(url, headers=headers, params=params)
             response.raise_for_status()
@@ -44,22 +55,23 @@ def process_data_for_date(date, prev_ff_pl_team_unlock):
 
             if response.status_code == 200 and data['code'] == 0 and len(data['data']) > 0:
                 for entry in data['data']:
-                    stat_date = entry['stat_date']
                     circulating_fil = entry['circulating_fil']
                     mined_fil = entry['mined_fil']
-                    vested_fil = entry['vested_fil']
-                    reserve_disbursed_fil = entry['reserve_disbursed_fil']
-                    locked_fil = entry['locked_fil']
                     burnt_fil = entry['burnt_fil']
 
                     # Calculate potential unlocked FIL
-                    potential_unlocked_fil = circulating_fil - mined_fil
+                    potential_unlocked_fil = circulating_fil - mined_fil - burnt_fil
+                    start_date = datetime.strptime("2020-10-15", '%Y-%m-%d')
 
-                    # Calculate FF/PL/Team Unlock (66.667% of potential unlocked FIL)
-                    ff_pl_team_unlock = 0.66667 * potential_unlocked_fil
+                    foundation_allocation = 0.05 * 2_000_000_000  # 5% of 2B FIL
+                    labs_allocation = 0.15 * 2_000_000_000  # 15% of 2B FIL
 
-                    # Calculate daily FF/PL/Team Unlock (difference from previous day)
-                    daily_ff_pl_team_unlock = ff_pl_team_unlock - prev_ff_pl_team_unlock
+                    # Calculate the daily vested FIL for Filecoin Foundation and Protocol Labs
+                    foundation_daily_vest = daily_vested_amount(start_date, date, foundation_allocation, 6)
+                    labs_daily_vest = daily_vested_amount(start_date, date, labs_allocation, 6)
+
+                    # Now calculate the FF/PL/Team Unlock for the day
+                    daily_ff_pl_team_unlock = foundation_daily_vest + labs_daily_vest
 
                     # Get FIL price from the loaded prices
                     fil_price = fil_prices[date]
@@ -67,37 +79,30 @@ def process_data_for_date(date, prev_ff_pl_team_unlock):
                     # Calculate FF/PL Daily Value based on unlocked FIL and FIL price
                     ff_pl_daily_value = daily_ff_pl_team_unlock * fil_price
 
-                    # Data processing succeeded, break out of the retry loop
-                break  # Data processing succeeded, break out of the retry loop
+                break
             else:
                 print("Error in API response:", data.get('message', 'Unknown error'))
                 retries += 1
                 time.sleep(60)
-
         except requests.exceptions.RequestException as e:
             print("Error making API request:", e)
             retries += 1
             time.sleep(60)
 
-    # If retries reached the maximum limit, we can return None or raise an exception
     if retries == max_retries:
         print(f"Failed to fetch data for date: {formatted_date} after {max_retries} attempts.")
         return None, prev_ff_pl_team_unlock
 
     processed_data = {
-        'stat_date': stat_date,
+        'stat_date': formatted_date,
         'circulating_fil': circulating_fil,
         'mined_fil': mined_fil,
-        'vested_fil': vested_fil,
-        'reserve_disbursed_fil': reserve_disbursed_fil,
-        'locked_fil': locked_fil,
         'burnt_fil': burnt_fil,
-        'ff_pl_team_unlock': ff_pl_team_unlock,
         'daily_ff_pl_team_unlock': daily_ff_pl_team_unlock,
         'fil_price': fil_price,
         'ff_pl_daily_value': ff_pl_daily_value,
     }
-    return processed_data, ff_pl_team_unlock  # Return the updated value for the next day
+    return processed_data, daily_ff_pl_team_unlock
 
 # Load FIL prices from CSV
 fil_prices = load_film_prices_from_csv('fil_prices.csv')
@@ -112,14 +117,12 @@ processed_data_dict = {}
 for date in fil_prices:
     processed_data, prev_ff_pl_team_unlock = process_data_for_date(date, prev_ff_pl_team_unlock)
     if processed_data:  # Only update if we successfully got the data for that date
-        prev_ff_pl_team_unlock = processed_data['ff_pl_team_unlock']
-    processed_data_dict[date] = processed_data
-
+        processed_data_dict[date] = processed_data
 
 # Define a function to write the processed data to a CSV file
 def write_processed_data_to_csv(data, csv_filename):
     with open(csv_filename, 'w', newline='') as csv_file:
-        fieldnames = ['Stat Date', 'Circulating FIL', 'Mined FIL', 'Vested FIL', 'Reserve Disbursed FIL', 'Locked FIL', 'Burnt FIL', 'FF/PL/Team Unlock', 'Daily FF/PL/Team Unlock', 'FIL Price', 'FF/PL Daily Value']
+        fieldnames = ['Stat Date', 'Circulating FIL', 'Mined FIL', 'Burnt FIL', 'Daily FF/PL/Team Unlock', 'FIL Price', 'FF/PL Daily Value']
         csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         csv_writer.writeheader()
 
@@ -128,11 +131,7 @@ def write_processed_data_to_csv(data, csv_filename):
                 'Stat Date': processed_data['stat_date'],
                 'Circulating FIL': processed_data['circulating_fil'],
                 'Mined FIL': processed_data['mined_fil'],
-                'Vested FIL': processed_data['vested_fil'],
-                'Reserve Disbursed FIL': processed_data['reserve_disbursed_fil'],
-                'Locked FIL': processed_data['locked_fil'],
                 'Burnt FIL': processed_data['burnt_fil'],
-                'FF/PL/Team Unlock': processed_data['ff_pl_team_unlock'],
                 'Daily FF/PL/Team Unlock': processed_data['daily_ff_pl_team_unlock'],
                 'FIL Price': processed_data['fil_price'],
                 'FF/PL Daily Value': processed_data['ff_pl_daily_value'],
